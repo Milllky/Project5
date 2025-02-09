79044ebb7645d26e3d56945b5114a52621fe218b
Trofimov M <milllky@rocketmail.com>
2025-02-09 19:42:34 +0500

    Add param test

--- src/test/java/ru/netology/services/VacationServiceTest.java
+++ src/test/java/ru/netology/services/VacationServiceTest.java
@@ -2,33 +2,19 @@ package ru.netology.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class VacationServiceTest {
 
-    @Test
-    void shouldCalculateVacation() {
+    @ParameterizedTest
+    @CsvSource({
+            "10000,3000,20000,3",
+            "100000,60000,150000,2"
+    })
+    void shouldCalculateVacation(int income, int expenses, int threshold, int expected) {
         VacationService service = new VacationService();
-        int income = 10_000;
-        int expenses = 3_000;
-        int threshold = 20_000;
-        int expected = 3;
-
         int actual = service.calculate(income, expenses, threshold);
-
         Assertions.assertEquals(expected, actual);
     }
-
-    @Test
-    void shouldCalculateVacation2() {
-        VacationService service = new VacationService();
-        int income = 100_000;
-        int expenses = 60_000;
-        int threshold = 150_000;
-        int expected = 2;
-
-        int actual = service.calculate(income, expenses, threshold);
-
-        Assertions.assertEquals(expected, actual);
-    }
-
 }
